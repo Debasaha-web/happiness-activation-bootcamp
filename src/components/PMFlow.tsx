33cd7e77165da7558a8d0d2e96f4746217a2dcbe
Debasaha-web <debasis.deep@gmail.com>
2026-06-24 12:02:42 +0600

    Update PMFlow.tsx

--- src/components/PMFlow.tsx
+++ src/components/PMFlow.tsx
@@ -147,7 +147,7 @@ export default function PMFlow({ day }: { day: Day }) {
                 <button
                   className="btn btn-lime btn-block"
                   style={{ marginTop: 22 }}
-                  onClick={() => { if (document.referrer) { window.location.href = document.referrer; } else { window.close(); } }}
+                  onClick={() => { history.back(); }}
                 >
                   Close Window
                 </button>
